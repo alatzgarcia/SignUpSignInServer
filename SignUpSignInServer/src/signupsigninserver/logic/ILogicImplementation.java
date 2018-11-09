659a7bc769ef30b598b2a88a9ee255938d32ac2b
NereaJimenez <43471171+NereaJimenez@users.noreply.github.com>
2018-11-09 22:44:49 +0100

    Update ILogicImplementation.java

--- SignUpSignInServer/src/signupsigninserver/logic/ILogicImplementation.java
+++ SignUpSignInServer/src/signupsigninserver/logic/ILogicImplementation.java
@@ -1,77 +1,80 @@
-/*
+//*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package signupsigninserver.logic;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import signupsignin.Message;
-import signupsignin.User;
+import signupsigninserver.databaseAccess.ConnectionPool;
 import signupsigninserver.databaseAccess.DAO;
 import signupsigninserver.databaseAccess.IDAO;
 import signupsigninserver.databaseAccess.IDAOFactory;
+import signupsigninserver.exceptions.NotAvailableConnectionsException;
 
 
 /**
- *
+ * Class for the server logic implementation and the server socket 
  * @author Nerea Jimenez
  */
 public class ILogicImplementation {
-    private static final Logger LOGGER = Logger.getLogger("signupsigninserver.ILogicImplementation");
-    private static int PORT = 5001;
+    private static final Logger LOGGER = 
+            Logger.getLogger("signupsigninserver.ILogicImplementation");
+    private static int maxThreads;
+    private static int port;
+    private static int threadsnum;
+    private static int connections;
     
-    public void start(){
-                LOGGER.info("Inicio");
+    /**
+     * Method that creates a ServerSocket and waits for connections
+     * It creates a thread for each client
+     */
+    public void start() throws NotAvailableConnectionsException{
+                               
                 ServerSocket server = null; 
-		Socket client = null; 
+                Socket client = null; 
 		
-                
+                //Asks the IDAOFactory for a DAO object
                 IDAO dao = IDAOFactory.getDAO();
 		try{ 
-                    
-                    server = new ServerSocket(PORT);
+                    getData();
+                    server = new ServerSocket(port);
+                    //the server keeps waiting for connections 
                     while(true){
                         
-                         LOGGER.info("Awaiting for client connection on port: " + PORT);
+                        LOGGER.info("Awaiting for client connection on port: " + port);
                         client = server.accept();
                         LOGGER.info("Client connected to the application.");
-                        Thread thread = new Thread(new LogicThread(client, (DAO) dao));
-                        thread.start();
-			
-                        /*LOGGER.info("Esperando conexiones");
-			//server waits for connections
-			client = server.accept(); 
-			//a client connects 
-                        LOGGER.info("Conectado");
-			ois = new ObjectInputStream(client.getInputStream()); 
-			oos = new ObjectOutputStream(client.getOutputStream());
-			Message message = (Message) ois.readObject(); 
-                        //String message = (String)ois.readObject();
-                        LOGGER.info("Mensaje recibido");
-			LOGGER.info(message.getMessage());*/
-                        
-                        
-                                       
-                       //oos.writeObject("ok");                          
-                                                
                        
+                        //when a client connects, a thread is created and it 
+                        //takes charge of the execution
+                        if(connections<maxThreads){
+                            Thread thread = new Thread(
+                                    new LogicThread(client, (DAO) dao));
+                            threadsnum++;
+                            thread.start();
+                            threadsnum--;
+                        }else
+                            throw new NotAvailableConnectionsException();
+                        }
                         
                         
-                    }
+                        
+                    
 		} catch (Exception e) {
 			
 		}  finally{
                     try{
                         
                         if(client!=null){
-                            client.close();
+                           client.close();
                         }
                         if(server!=null){
                             server.close();
@@ -83,10 +86,38 @@ public class ILogicImplementation {
     }
         
 	
-
-    public static void main(String[] args) {
+    /**
+     * 
+     * @param args 
+     */
+    public static void main(String[] args) throws NotAvailableConnectionsException {
         ILogicImplementation server = new ILogicImplementation();
         server.start();
+    }
+    /**
+     * Method that takes the parameters from the config file
+     */
+    private void getData() {
+        Properties config = new Properties();
+	FileInputStream input = null;
+	try {
+            input = new FileInputStream("src/signupsigninserver/config/connection.properties");
+            config.load(input);
+            port=Integer.parseInt(config.getProperty("port"));
+            maxThreads=Integer.parseInt(config.getProperty("max_threads"));
+           
+         } catch (FileNotFoundException ex) {
+            LOGGER.info("No encuentra archivo");
+        } catch (IOException ex) {
+            Logger.getLogger(ILogicImplementation.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (input != null)
+		try {
+                    input.close();
+            } catch (IOException ex) {
+                Logger.getLogger(ILogicImplementation.class.getName()).log(Level.SEVERE, null, ex);
+            } 
+	}
     }
 
     
